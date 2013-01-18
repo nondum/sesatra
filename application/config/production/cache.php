8feb65bda9c8b251095797af3c8c707cb8b649df
Twaambo Haamucenje <twaambo@outlook.com>
2013-01-18 18:39:11 +0200

    server customisations
    - production configs

--- application/config/production/cache.php
+++ application/config/production/cache.php
@@ -2,6 +2,6 @@
 
 return array(
 
-    'driver' => 'memcached',
+    'driver' => 'file',
 
 );
--- application/config/production/database.php
+++ application/config/production/database.php
@@ -6,10 +6,10 @@ return array(
     'connections' => array(
         'production' => array(
             'driver'   => 'mysql',
-            'host'     => $_SERVER['DB1_HOST'],
-            'database' => $_SERVER['DB1_NAME'],
-            'username' => $_SERVER['DB1_USER'],
-            'password' => $_SERVER['DB1_PASS'],
+            'host'     => 'localhost',
+            'database' => $_SERVER['THADBCONN'],
+            'username' => $_SERVER['THADBUSER'],
+            'password' => $_SERVER['THADRAGON'],
             'charset'  => 'utf8',
             'prefix'   => '',
         ),
